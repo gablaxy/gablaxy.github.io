8845cb1cb1c2bd6915b3235bd9c5c2ed4562491a
glavazais <lavazaisgabin@gmail.com>
2022-11-27 00:41:57 +0100

    affichage des feed seulement avec des articles récents

--- app.js
+++ app.js
@@ -9,22 +9,33 @@
   
 // for each feed in the feeds array we do the following
   feeds.forEach(function (url) {
-    feednami.load(url)
-    .then(feed => {
-      let ul = document.createElement('ul');
-      ul.innerHTML = '<h2>' + feed.meta.title + '</h2>'; // we add the feed title
-      textarea.appendChild(ul);
+    feednami.load(url).then(feed => {
 
-      feed.entries.forEach(entry => {
-        let date = new Date(entry.pubDate);
-        if (date > day_minus_five()) { // if it's less than 5 days old we add it to the page
-          let li = document.createElement('li');
-          li.innerHTML = `<h4><a href="${entry.link}">${entry.title}</a></h4>`;
-          textarea.appendChild(li);
-        }
+      let newarticle = false; 
+      feed.entries.forEach(entry => { // we check if there is at least one article that is newer than 5 days
+          let date = new Date(entry.pubDate);
+          if (date > day_minus_five()) {
+            newarticle = true;
+            return newarticle;
+          }
       });
-    })
+
+      if (newarticle) { // if there is at least one article that is newer than 5 days we display the feed
+        let ul = document.createElement('ul');
+        ul.innerHTML = '<h2>' + feed.meta.title + '</h2>'; // we add the feed title
+        textarea.appendChild(ul);
+        feed.entries.forEach(entry => {
+          let date = new Date(entry.pubDate);
+          if (date > day_minus_five()) { // if it's less than 5 days old we add it to the page
+            let li = document.createElement('li');
+            li.innerHTML = `<h4><a href="${entry.link}" target="_blank">${entry.title}</a></h4>`;
+            textarea.appendChild(li);
+          }
+        });
+      }
+    });
   });
+
 
 
 
@@ -34,6 +45,5 @@
     dateminus5.setDate(dateminus5.getDate() - 5);
     return dateminus5;
   }
-
 //Using feednami to fetch RSS feeds
 //https://toolkit.sekando.com/docs/en/feednami
