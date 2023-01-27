b7294df9a9a978bcfc8847e06bceda53279672ab
glavazais <lavazaisgabin@gmail.com>
2023-01-27 18:10:14 +0100

    css changement + abbréviations titres feeds

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
+const textarea = document.querySelector('#feed-textarea > ul');
+const datedujour = new Date();
 
-  const textarea = document.querySelector('#feed-textarea > ul');
+console.log(datedujour);
 
-  const datedujour = new Date();
-  console.log(datedujour);
-  const datedujourmoins5 = day_minus_five();
-  document.querySelector('#date').innerHTML = datedujourmoins5.toLocaleDateString() + ' - ' + datedujour.toLocaleDateString();
-  let feeds = ['https://nouveaupartianticapitaliste.org/rss.xml','https://unioncommunistelibertaire.org/spip.php?page=backend','http://mouvement-municipal.fr/feed/'];
+const datedujourmoins5 = day_minus_five();
+  
+document.querySelector('#date').innerHTML = datedujourmoins5.toLocaleDateString() + ' - ' + datedujour.toLocaleDateString();
+let feeds = ['https://nouveaupartianticapitaliste.org/rss.xml','https://unioncommunistelibertaire.org/spip.php?page=backend','http://mouvement-municipal.fr/feed/','https://www.cnt-f.org/spip.php?page=backend','https://workingclasshistory.com/feed/','https://bibliothequelibertad.noblogs.org/feed/'];
 
   
 // for each feed in the feeds array we do the following
   feeds.forEach(function (url) {
     feednami.load(url).then(feed => {
-
+      console.log(feed.meta.title)
+      console.log(nom_raccourci(feed.meta.title))
       let newarticle = false; 
       feed.entries.forEach(entry => { // we check if there is at least one article that is newer than 5 days
           let date = new Date(entry.pubDate);
@@ -22,14 +24,12 @@
       });
 
       if (newarticle) { // if there is at least one article that is newer than 5 days we display the feed
-        let ul = document.createElement('ul');
-        ul.innerHTML = '<h2>' + feed.meta.title + '</h2>'; // we add the feed title
-        textarea.appendChild(ul);
         feed.entries.forEach(entry => {
+          let nom_court = nom_raccourci(feed.meta.title);
           let date = new Date(entry.pubDate);
           if (date > day_minus_five()) { // if it's less than 5 days old we add it to the page
             let li = document.createElement('li');
-            li.innerHTML = `<h4><a href="${entry.link}" target="_blank">${entry.title}</a></h4>`;
+            li.innerHTML = `<h4>${nom_court}\t<a href="${entry.link}" target="_blank">${entry.title}</a></h4>`;
             textarea.appendChild(li);
           }
         });
@@ -38,7 +38,18 @@
   });
 
 
+ function nom_raccourci(title){
+    let list_noms = {
+      "UCL - Union communiste libertaire" : "UCL",
+      "Mouvement Municipal" : "MM",
+      "Confédération nationale du travail" : "CNT",
+      "Working Class History" : "WCH",
+      "Bibliothèque anarchiste Libertad" : "BAL",
+      "NPA" : "NPA"
+    }
 
+    return list_noms[title];
+ }
 
 
   function day_minus_five(){ // function to get the date of 5 days ago
@@ -48,3 +59,15 @@
   }
 //Using feednami to fetch RSS feeds
 //https://toolkit.sekando.com/docs/en/feednami
+
+
+const containers = [ "main", "projects", "prog" ];
+function showContainer(container) {
+    for (let i = 0; i < containers.length; i++) {
+        const sel = containers[i] === container;
+        document.getElementById(containers[i]).style.display = sel ? "block" : "none";
+        document.getElementById(containers[i] + "-nav").style.color = sel ? "var(--header-color)" : null;
+    }
+}
+
+showContainer("main");
