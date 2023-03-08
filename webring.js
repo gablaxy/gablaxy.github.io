8db20e598decc90b807f1ffcaa43a2f235facf06
glavazais <lavazaisgabin@gmail.com>
2023-03-08 23:50:38 +0100

    ajout webring

---
+++ webring.js
@@ -0,0 +1,69 @@
+const JSON_WEBRING = "https://gablaxy.github.io/webring.json";
+
+const template = document.createElement("template");
+template.innerHTML = `
+<style>
+#webring-inner a{
+    color: var(--titres);
+    text-decoration: none;
+}
+
+#webring-inner a:hover{
+    color: var(--hover);
+}
+
+#webring-inner{
+    color: var(--texte);
+}
+
+</style>
+
+<br>
+<b class="black" id="webring-inner">
+    <!-- Webring content -->
+</b>
+<br>
+`;
+
+class Webring extends HTMLElement {
+    connectedCallback() {
+        this.attachShadow({ mode: "open" });
+
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+
+        const currentSite = this.getAttribute("current-site");
+
+        fetch(JSON_WEBRING)
+            .then((response) => response.json())
+            .then((sites) => {
+                // Va chercher le site actuel dans le json
+                const matchedSiteIndex = sites.findIndex(
+                    (site) => site.name === currentSite
+                );
+                const matchedSite = sites[matchedSiteIndex];
+
+                // Va chercher le site précédent dans le json
+                const previousSiteIndex = matchedSiteIndex - 1;
+                if(previousSiteIndex < 0)previousSiteIndex = sites.length - 1;
+                
+                const nextSiteIndex = matchedSiteIndex + 1;
+                if(nextSiteIndex >= sites.length)nextSiteIndex = 0;
+
+                const randomSiteIndex = Math.floor(Math.random() * sites.length);
+
+                const content = `
+                <a href="${sites[previousSiteIndex].url}" rel="prev noreferrer external">&lt; avant</a>
+                / part of the 
+               <a rel="external noreferrer" href="index.html">nom du ring</a>
+                 webring / 
+               <a href="${sites[nextSiteIndex].url}" rel="next noreferrer external">après &gt;</a>
+                `;
+
+                this.shadowRoot
+                    .querySelector("#webring-inner")
+                    .insertAdjacentHTML("afterbegin", content);
+            });
+    }
+}
+
+window.customElements.define("webring-iutechno", Webring);
