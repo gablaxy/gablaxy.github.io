5f56b964e5fb1afe59f20a4e1fd8eb308289e7b0
gablaxy <32848524+gablaxy@users.noreply.github.com>
2023-06-06 11:44:44 +0200

    Create articles.js

---
+++ articles/articles.js
@@ -0,0 +1,12 @@
+${document}.ready(function(){
+                        let list_li = ["test","yuy"];
+                        let articles = document.getElementById("liste_arti");
+                        for(let i = 0; i < list_li.length; i++){
+                            let content = document.createElement("li");
+                            content.innerHTML = `<a href="./${list_li[i]}.html">${list_li[i]}</a>`
+                            console.log(i);
+                            console.log(list_li[i]);
+                            console.log(content);
+                            articles.appendChild(content);
+                        }
+})
