bdaae8fee2e953f93666df9e0dc199ed1fc9f571
gablaxy <gab2003video@gmail.com>
2023-11-04 22:57:40 +0100

    restructuration des fichiers et ajout background

---
+++ livres.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="favicon" href="img/favicon.ico">
+    <link rel="font" href="fonts/Poppins/Poppins-Black.ttf">
+    
+    <link rel="stylesheet" type="text/css" href="./style/style.css">
+    <link rel="stylesheet" type="text/css" href="./style/common.css">
+    <title>Books</title>
+</head>
+<body>
+    <?php include_once "./header.html" ?>
+    <div id="canvas" class="box" style="max-width: 2000px;">
+        
+        <div id="main">
+            <ul>
+                <li><a href="/livres/report.PDF">example pdf</a></li>
+                <li><a href="/livres/report.PDF">example pdf</a></li>
+                <li><a href="/livres/report.PDF">example pdf</a></li>
+                <li><a href="/livres/report.PDF">example pdf</a></li>
+            </ul>
+        </div>
+    </div>
+    <?php include_once "./footer.html" ?>
+    <script src="https://static.sekandocdn.net/static/feednami/feednami-client-v1.1.js"></script>
+</body>
+</html>
