6fabb692437126230ef5c43a00b0f27f0cc104e7
gablaxy <gab2003video@gmail.com>
2024-01-07 20:36:34 +0100

    fix ?

--- stellar_system/click.js
+++ stellar_system/click.js
@@ -1,6 +1,7 @@
 let objets = document.querySelectorAll(".all > div");
 let menu = document.querySelector(".menu");
 let all = document.querySelector(".all");
+const api_key = "kH/cxvN9hN/tF6Ypm8gwkQ==HWZR42Yx2TgJKFyO";
 
 function removeSA() {
     objets.forEach(element => {
@@ -8,11 +9,32 @@ function removeSA() {
     });
 }
 
+// Fonction qui récupère les informations sur la planete en question dans l'API https://api.api-ninjas.com/v1/planets?name= et les renvoie sous forme de string, attend la fin de la requête pour renvoyer la valeur
+function getinfo(satellite) {
+    let url = "https://api.api-ninjas.com/v1/planets?name=" + satellite;
+    let request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.setRequestHeader("X-Api-Key", api_key);
+    request.send();
+    console.log("request sent");
+    // the result is an object
+    request.onload = function() {
+        let result = JSON.parse(request.response);
+        console.log(result);
+        let description = "Mass: " + result[0].mass + " kg<br>Radius: " + result[0].radius + " km<br>Period: " + result[0].period + " days<br>Temperature: " + result[0].temperature + " °C<br>Host star: " + result[0].host_star_mass;
+        document.getElementById("desc").innerHTML = description;
+    }
+}
+
+
 objets.forEach(element => {
     element.addEventListener("click", function(){
         removeSA();
         menu.style.display = "block";
         element.classList.add("satellite_active");
         document.getElementById("nom").innerHTML = element.classList[0];
+        console.log(element.classList[0]);
+        getinfo(element.classList[0]);
     });
 });
+
