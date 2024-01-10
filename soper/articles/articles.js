5e54b85e85f35fc326e905f40de44bd9c17debd8
gablaxy <32848524+gablaxy@users.noreply.github.com>
2024-01-10 17:00:07 +0100

    Update articles.js

--- soper/articles/articles.js
+++ soper/articles/articles.js
@@ -1,7 +1,9 @@
 let list_li = ["future_space","hello world"];
+let list_date = ["November 15, 2023","July 8, 2023"];
 let articles = document.getElementById("liste_arti");
+
 for(let i = 0; i < list_li.length; i++){
 	let content = document.createElement("li");
-	content.innerHTML = `<a href="./${list_li[i]}.html">${list_li[i]}</a>`
+	content.innerHTML = `<pre>${list_date[i]}</pre><a href="./${list_li[i]}.html">${list_li[i]}</a>`
 	articles.appendChild(content);
 }
